55a8ba08b1620baf18b17763183a798c9115bb36
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-19 14:00:18 +0530

    Update AStudent.java

--- Session6Assignment4/src/AStudent.java
+++ Session6Assignment4/src/AStudent.java
@@ -3,5 +3,5 @@ public  abstract class AStudent {
 	int Studentroll;
 	String name;
 	int regnumber;
-public abstract void result(int m1,int m2,int m3,String name);
+public abstract void result(int m1,int m2,int m3);
 }
