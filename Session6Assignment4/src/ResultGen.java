c34c77738fc792dd2c02b344eddfa0045736ca3a
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-19 13:59:45 +0530

    Update ResultGen.java

--- Session6Assignment4/src/ResultGen.java
+++ Session6Assignment4/src/ResultGen.java
@@ -2,22 +2,22 @@
 public class ResultGen extends AStudent{
 
 	
-	public void result(int m1,int m2,int m3,String name) {
+	public void result(int m1,int m2,int m3) {
 		
 		// TODO Auto-generated method stub
 		if(m1<35)
 		{
-			System.out.println("Result" +name +"is fail");
+			System.out.println("Result is fail");
 		}
 		
 			else if(m2<35){
-				System.out.println("Result" +name +"is fail");
+				System.out.println("Result is fail");
 			}
-				else if(m2<35){
-					System.out.println("Result" +name +"is fail");
+				else if(m3<35){
+					System.out.println("Result is fail");
 				}
 				else
-					System.out.println("Result" +name +"is pass");
+					System.out.println("Result is pass");
 			}
 	
 
